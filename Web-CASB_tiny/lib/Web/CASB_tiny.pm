c850572b48169f8f68281f01fc7313c964d1bedc
Peter M Allan <peabrain@notatla.org.uk>
2017-04-19 17:59:33 +0100

    run as fcgi, serve static files from default route

--- Web-CASB_tiny/lib/Web/CASB_tiny.pm
+++ Web-CASB_tiny/lib/Web/CASB_tiny.pm
@@ -17,20 +17,8 @@ get '/' => sub {
 # collect uploads into a record for this session
 # finalise and logoff
 
-get '/count' => sub {
-    my $counter = session('cnt');
-    debug("Counter from session: $counter");
-    $counter++;
-    session('cnt' => $counter);
-};
-
-get '/a_static_page' => sub {
-        send_file '/some_static.html'
-};
-
 get '/logout' => sub {
      app->destroy_session;
-     set_flash('You are logged out.');
      redirect '/';
 };
 
@@ -42,20 +30,6 @@ get '/upload' => sub {
 post '/upload' => sub {
     my $data = request->upload('file');
 
-    my $dir = path(config->{appdir}, 'uploads');
-    mkdir $dir if not -e $dir;
-
-    if (!defined(session('subdir'))) {
-        session('subdir' => (int rand(1_000_000)));
-    }
-    my $half = path($dir, session('subdir'));
-    mkdir $half if not -e $half;
-
-    my $path = path($half, $data->basename);
-    if (-e $path) {
-        return "'$path' already exists";
-    }
-
     # What did the form get and does it match the session?
     my $form_csrf = request->{body_parameters}->{csrf_token};
     if ($form_csrf ne session('csrf')) {
@@ -64,16 +38,53 @@ post '/upload' => sub {
         return template 'form_failed';
     }
 
+    my $dir = path(config->{appdir}, 'uploads');
+    mkdir $dir if not -e $dir;
+    if (!defined(session('subdir'))) {
+        session('subdir' => (int rand(1_000_000)));
+    }
+    my $half = path($dir, session('subdir'));
+    mkdir $half if not -e $half;
+    my $path = path($half, $data->basename);
+    if (-e $path) {
+        return "'$path' already exists";
+    }
+
     $data->link_to($path) or return "Link failed :$path: $!";
-    #
+
     my $sess_file_list=session('file_list');
     $sess_file_list="" if (!defined($sess_file_list));
     $sess_file_list .= "<br />";
     $sess_file_list .= $data->basename;
     session('file_list' => $sess_file_list);
-    #
+
     template 'progress' => { list => session('file_list') };
 };
 
+get '/finish' => sub {
+     my $trigger_file = 'uploads/' . session('subdir') . '/_.txt';
+     open(my $tf, '>', $trigger_file) or die("open $!");
+     foreach my $n (1..3) {
+         printf($tf "Number %d\n", $n);
+     }
+     printf($tf "END\n");
+     close($tf) or die("close $!");
+     app->destroy_session;
+     redirect '/';
+};
+
+# default - either serve a static file or not_found
+any qr{.*} => sub {
+    if (request->path =~ m{^(/[/\w-]+.?\w+)$}) {
+        if ( -f "public/www".$1) {
+           return send_file "www".$1;
+        } else {
+            status 'not_found';
+            template 'special_404', { path => $1 };
+        }
+    }
+    status 'not_found';
+    template 'special_404', { path => 'something with special characters in' };
+};
 
 true;
